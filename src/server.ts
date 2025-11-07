495bca3e809e0e7fe33aaa4f5588dee260191c08
Eduardo Tashiro <eduardogaia854@hotmail.com>
2025-11-07 12:57:35 -0300

    feat: server initialization completed

--- src/server.ts
+++ src/server.ts
@@ -1,1 +1,12 @@
 //inicializar o servidor
+import { app } from "./app";
+
+(async () => {
+  const port = process.env.PORT || 3000;
+  try {
+    await app.start(port);
+    app.logger.info(` app is running! ${port}`);
+  } catch (error) {
+    app.logger.info(`app is not running! :c `);
+  }
+})();
