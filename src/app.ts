4807c51f7b15d330cc34303bda0267febe92d99c
Eduardo Tashiro <eduardogaia854@hotmail.com>
2025-11-16 02:58:16 -0300

    feat: initialize webhook :v

--- src/app.ts
+++ src/app.ts
@@ -1,13 +1,30 @@
-import { App } from "@slack/bolt";
+import { App,ExpressReceiver,LogLevel } from "@slack/bolt";
 import {config} from "./config/env"
+import express from "express";
 
+//https://github.com/slackapi/bolt-js/blob/main/examples/oauth-express-receiver/app.js
+//O PagerDuty espera uma resposta 2xx em até 5 segundos para webhooks genéricos e em até 16 segundos para webhooks gerados a partir de ações de incidentes personalizadas.
+                            
+export const receiver = new ExpressReceiver({
+  signingSecret:config.slack.signingSecret,
+  clientId:config.slack.clientID,
+  clientSecret:config.slack.clientSecret
+})
 
 export const app = new App({
-  token: config.slack.botToken,
-  signingSecret: config.slack.signingSecret,
-});                                    
+  receiver,
+  logLevel:LogLevel.INFO, // set loglevel at the App level
+  token: config.slack.botToken
+});     
 
+receiver.app.use(express.json());
 
+receiver.app.post('/webhook/pagerduty', (req, res) => {
+
+  console.log("retorno do pager teste", req.body);
+  
+  res.status(200).send('tudo safe');
+});
 
 
 import "./appSlack/events/reaction"   ;
